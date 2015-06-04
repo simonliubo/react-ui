ce5443b5da6d04985c4877c6ce8f8f082e5819d9
liubo <liubo@beisen.com>
2015-06-04 16:01:19 +0800

    init

--- examples/scripts/views/home/index.jsx
+++ examples/scripts/views/home/index.jsx
@@ -1,37 +1,40 @@
 /**
  * Created by liubo on 15/3/12.
  */
-var Button = ReactBootstrap.Button;
-var ButtonToolbar = ReactBootstrap.ButtonToolbar;
+//var Button = ReactBootstrap.Button;
+//var ButtonToolbar = ReactBootstrap.ButtonToolbar;
 
 var Waves = require('../../waves');
 
-var Button = ReactBootstrap.Button;
+//var Button = ReactBootstrap.Button;
 var App = React.createClass({
     mixins: [ Waves ]
     ,render: function() {
 
-
         return(
-            <ButtonToolbar>
-                <Button href="javascript:void(0)"
-                        onMouseDown={this._onMouseDown}
-                        onMouseUp={this._onMouseUp}
-                        onMouseLeave={this._onMouseLeave}
-                        onClick={this._onClick}
-                        className="waves-effect waves-light"
-                    >Default</Button>
-                <Button>Default</Button>
-                <Button bsStyle='primary'
-                        onMouseDown={this._onMouseDown}
-                        onMouseUp={this._onMouseUp}
-                        onMouseLeave={this._onMouseLeave}
-                        onClick={this._onClick}
-                        className="waves-effect waves-light"
-                    >Default</Button>
-                <Button bsStyle='warning'>Warning</Button>
-            </ButtonToolbar>
+            <div>test</div>
         );
+
+        //return(
+        //    <ButtonToolbar>
+        //        <Button href="javascript:void(0)"
+        //                onMouseDown={this._onMouseDown}
+        //                onMouseUp={this._onMouseUp}
+        //                onMouseLeave={this._onMouseLeave}
+        //                onClick={this._onClick}
+        //                className="waves-effect waves-light"
+        //            >Default</Button>
+        //        <Button>Default</Button>
+        //        <Button bsStyle='primary'
+        //                onMouseDown={this._onMouseDown}
+        //                onMouseUp={this._onMouseUp}
+        //                onMouseLeave={this._onMouseLeave}
+        //                onClick={this._onClick}
+        //                className="waves-effect waves-light"
+        //            >Default</Button>
+        //        <Button bsStyle='warning'>Warning</Button>
+        //    </ButtonToolbar>
+        //);
 
     }
     ,_onMouseDown: function(e){
---
+++ webpack.examples.config.js
@@ -0,0 +1,3 @@
+module.exports = require("./webpack.make.examples.config")({
+    devServer: true
+});
