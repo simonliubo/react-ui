5903ffabab6d1e9e7daa303062a6ed2c0b11d724
liubo <liubo@beisen.com>
2015-07-06 10:11:06 +0800

    update

---
+++ examples/test/requestanimframe.js
@@ -0,0 +1,15 @@
+/**
+ * Created by liubo on 15/7/3.
+ */
+// Big ol' <3'z to Paul Irish because, you know,
+// he's like a brother from another mother and stuff.
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
